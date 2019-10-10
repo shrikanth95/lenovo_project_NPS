0f8b5c720bbb716d0a35f6ba3905259a6f99d24c
shrikanth95 <sh5924th@gmail.com>
2019-10-10 00:41:50 -0400

    First Commit

---
+++ README.md
@@ -0,0 +1,1 @@
+Repository for ISE 560 Course
---
+++ src_lenovo_project.R
@@ -0,0 +1,34 @@
+library(lubridate)
+
+## Functions used for the lenovo project in ISE560
+# Description: Calculates the PIS of the sentiment data with each product. NOTE  "IDEAPAD 120S 14" and "IDEAPAD 120S 15" are considered to be two different products
+# Input: CIS dataset
+# Output: PIS scores for each available product and year
+
+calculate.PIS <- function(cis.working) {
+
+  list.products = unique(cis.working$Product)
+  
+  pis.working <- data.frame(Product = character(length(list.products)), 
+                            Year.make = character(length(list.products)), 
+                            PIS = numeric(length(list.products)), 
+                            sample.size = numeric(length(list.products)),
+                            stringsAsFactors = FALSE)
+  row = 1
+  for(prod in list.products){
+      cis.slice = cis.working[cis.working$Product == prod, ]
+      num.neg = length(cis.working$Sentiment=="NEGATIVE")
+      num.pos = length(cis.working$Sentiment=="POSITIVE")
+      pis.working$Product[row] = prod
+      pis.working$Year.make[row] = ifelse(!is.null(strsplit(cis.slice$Product[1], " ")[[1]][3]), 
+                                          yes = strsplit(cis.slice$Product[1], " ")[[1]][3],
+                                          no = -1)
+      pis.working$sample.size[row] = nrow(cis.slice)
+      if(is.numeric(num.neg)&is.numeric(num.pos)) 
+        pis.working$PIS[row] = (num.pos-num.neg)/(num.pos+num.neg)*10
+      else 
+        pis.working$PIS[row] = -1
+      row = row + 1
+  }
+  return(pis.working)
+}
