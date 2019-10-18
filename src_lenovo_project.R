066d5991d6dddc36c26634bc5210f64afe82f72e
shrikanth95 <sh5924th@gmail.com>
2019-10-18 12:28:24 -0400

    Cleared bugs and added time of survey for CID working dataset

--- src_lenovo_project.R
+++ src_lenovo_project.R
@@ -7,28 +7,64 @@ library(lubridate)
 
 calculate.PIS <- function(cis.working) {
 
-  list.products = unique(cis.working$Product)
+  cis.working$Comment.time =  format(cis.working$Comment.Date, "%Y-%m")
   
-  pis.working <- data.frame(Product = character(length(list.products)), 
-                            Year.make = character(length(list.products)), 
-                            PIS = numeric(length(list.products)), 
-                            sample.size = numeric(length(list.products)),
+  list.products = unique(cis.working$Product)
+  list.comment.time = unique(cis.working$Comment.time)
+  len.working = length(list.products)*length(list.comment.time)
+  pis.working <- data.frame(Product = character(len.working), 
+                            Comment.time = character(len.working),
+                            PIS = numeric(len.working), 
+                            sample.size = numeric(len.working),
                             stringsAsFactors = FALSE)
   row = 1
+  
   for(prod in list.products){
-      cis.slice = cis.working[cis.working$Product == prod, ]
+    for(comment.time in list.comment.time){
+      cis.slice = cis.working[(cis.working$Product == prod)&(cis.working$Comment.time == comment.time), ]
       num.neg = length(which(cis.slice$Sentiment=="NEGATIVE"))
       num.pos = length(which(cis.slice$Sentiment=="POSITIVE"))
       pis.working$Product[row] = prod
-      pis.working$Year.make[row] = ifelse(!is.null(strsplit(cis.slice$Product[1], " ")[[1]][3]), 
-                                          yes = strsplit(cis.slice$Product[1], " ")[[1]][3],
-                                          no = -1)
+      pis.working$Comment.time[row] = comment.time
       pis.working$sample.size[row] = nrow(cis.slice)
       if(is.numeric(num.neg)&is.numeric(num.pos)) 
         pis.working$PIS[row] = (num.pos-num.neg)/(num.pos+num.neg)*10
       else 
         pis.working$PIS[row] = -1
       row = row + 1
+    }
   }
   return(pis.working)
+}
+
+# Description: Calculates the pNPS from NPS surveys for each product. NOTE  "IDEAPAD 120S 14" and "IDEAPAD 120S 15" are considered to be two different products
+# Input: pNPS dataset
+# Output: pNPS scores for each available product and year
+
+calculate.NPS <- function(nps.working) {
+  
+  list.products = unique(nps.working$Product)
+  
+  pnps.working <- data.frame(Product = character(length(list.products)), 
+                            pNPS = numeric(length(list.products)), 
+                            # bNPS = numeric(length(list.products)), 
+                            sample.size = numeric(length(list.products)),
+                            stringsAsFactors = FALSE)
+  row = 1
+  for(prod in list.products){
+    nps.slice = nps.working[nps.working$Product == prod, ]
+    pnps.working$Product[row] = prod
+    pnps.working$sample.size[row] = nrow(nps.slice)
+    if(is.numeric(nrow(nps.slice))){ 
+      num.prom = nrow(nps.slice[(nps.slice$Product.NPS>8)&(nps.slice$Product.NPS<11),])
+      num.det = nrow(nps.slice[(nps.slice$Product.NPS>-1)&(nps.slice$Product.NPS<7),])
+      pnps.working$pNPS[row] = num.prom - num.det
+    }
+    else{
+      pnps.working$pNPS[row] = NA
+      # pnps.working$bNPS[row] = -1
+    }
+    row = row + 1
+  }
+  return(pnps.working)
 }
